b167d2771e55c38ea3f00348ce9cddf5e82111de
SSPENELOPE <typoepping@gmail.com>
2023-03-07 10:36:18 -0500

    added login check to profile page

--- ClientApp/src/pages/Profile.jsx
+++ ClientApp/src/pages/Profile.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
 
 function Profile() {
-    
-    return (
-        <div>
 
-        </div>
-    )
+    const profileId = Auth.getProfile().userId;
+    console.log(profileId);
+    
+    // We first will check to see if the user is logged in, if they are not we will direct them to the login page
+    if (!Auth.loggedIn()) {
+        document.location.replace("/login");
+      }
+
+    if(Auth.loggedIn() && profileId) {
+        return (
+            <div>
+                <h1>hello world</h1>
+            </div>
+        )
+    }
+
+
 }
 
 export default Profile;

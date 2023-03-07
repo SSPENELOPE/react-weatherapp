f8bb67c9df2dc87e51f12b94a427aa237fff0cb1
SSPENELOPE <typoepping@gmail.com>
2023-03-07 12:31:17 -0500

    added react bootstrap to project, added offcanvas which will include the users saved/favorited cities later

--- ClientApp/src/pages/Profile.jsx
+++ ClientApp/src/pages/Profile.jsx
@@ -1,24 +1,56 @@
 import React, { useState, useEffect } from "react";
 import Auth from "../utils/auth";
 import ProfileHeader from "../components/ProfileHeader";
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function Profile() {
 
     const profileId = Auth.getProfile().userId;
-    console.log(profileId);
     const userName = Auth.getProfile().unique_name;
-    console.log(userName);
-    
+
+    // The offcanvas list of the users saved cities
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
     // We first will check to see if the user is logged in, if they are not we will direct them to the login page
     if (!Auth.loggedIn()) {
         document.location.replace("/login");
-      }
+    }
 
-    if(Auth.loggedIn() && profileId) {
+    // If we are logged in this is what we will display
+    if (Auth.loggedIn() && profileId) {
         return (
             <div>
+                {/* Render the header  */}
                 <ProfileHeader userName={userName} />
-                <h1>hello world</h1>
+                <div className="d-flex flex-row">
+                    {/* Render the sidenav bar with the users favorited cities */}
+                    <div className="sideNav">
+
+
+                        <Button variant="primary m-2" onClick={handleShow}>
+                            Change your city
+                        </Button>
+
+                        <Offcanvas show={show} onHide={handleClose}>
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                              <div>
+                                <h2>Hello World</h2>
+                              </div>
+                            </Offcanvas.Body>
+                        </Offcanvas>
+
+
+                    </div>
+                </div>
+
             </div>
         )
     }
